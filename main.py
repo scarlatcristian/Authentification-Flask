99c7e743c666fc8689236edd5bdd8c9d6e37069e
scarlat <scarlat.cristian.gabriel@gmail.com>
2024-01-28 19:10:24 +0200

    hash the password

--- main.py
+++ main.py
@@ -19,9 +19,6 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(1000))
 
 
-# Line below only required once, when creating DB.
-# db.create_all()
-
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -30,10 +27,16 @@ def home():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        print(hash_and_salted_password)
         new_user = User(
             email=request.form.get('email'),
             name=request.form.get('name'),
-            password=request.form.get('password'),
+            password=hash_and_salted_password,
         )
         db.session.add(new_user)
         db.session.commit()
